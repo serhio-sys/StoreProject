673d9a322484278532c7732b56b79b475be43997
ZXCMONSTER <wertunwertun@rambler.ru>
2022-09-11 15:50:19 +0300

    MEDIA_SOLVE

--- Settings/urls.py
+++ Settings/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 import debug_toolbar
 from Users.views import pageNotFound
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('shop/',include("Shop.urls")),
     path('orders/', include("Orders.urls")),
     path('__debug__/', include(debug_toolbar.urls)),
+    re_path(r'^media/(?P<path>,*)$',serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG is False:
